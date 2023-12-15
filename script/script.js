64646a15232469f1205753455015bdb0bdfa7a5e
Anton Adithya <122534649+antonadithya@users.noreply.github.com>
2023-12-15 22:08:04 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,68 @@
+
+// INPUT DATA  LIST * LIST DATA ADD TO BUTTON CLICK *
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+
+function addTask(){
+  if(inputBox.value == ""){
+    alert("You  must write something !")
+  }
+else{
+  let li = document.createElement("li");
+  li.innerHTML = inputBox.value ;
+  listContainer.appendChild(li);
+
+  
+  let span = document.createElement("span");
+  span.innerHTML = "\u00d7"
+  li.appendChild(span);
+}
+ inputBox.value = "";
+ saveData()
+
+}
+
+
+  document.addEventListener("keypress", function(event){
+
+ if (event.key === "Enter") {
+  addTask();
+ }
+});
+ 
+
+
+
+
+
+
+
+// INPUT DATA ADD IN LIST CONTAINER ** (DELETE /SELECT )** 
+
+listContainer.addEventListener("click", function(event){
+  if(event.target.tagName === "LI"){
+    event.target.classList.toggle("cheaked")
+    saveData()
+
+  }else if (event.target.tagName = "SPAN"){
+    event.target.parentElement.remove();
+    saveData()
+  }
+}, false)
+
+
+
+//Save all activity 
+
+function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+//display All activities
+
+function showTask(){
+  listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask();
